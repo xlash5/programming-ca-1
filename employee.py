98f1cd106fb907642fb429b49d7c5a92222434b0
Enes <menesilhan@gmail.com>
2022-04-09 16:26:30 +0100

    comments added

--- employee.py
+++ employee.py
@@ -1,4 +1,6 @@
 # StaffID, LastName, FirstName, RegHours, HourlyRate, OTMultiple, TaxCredit, StandardBand,
+# I tried to keep decimal places same as example
+# Moved constant values to different file. I thought it would be easier to read.
 import datetime
 from constants import *
 
@@ -27,18 +29,21 @@ class Employee:
         if hours_worked < 0:
             raise ValueError('Hours work must be positive')
 
+        # if hours worked exceeding reg_hours then returning max reg_hours
         reg_hours_worked = self.__reg_hours if self.__reg_hours <= hours_worked else hours_worked
         overtime_rate = int(self.__hourly_rate * self.__ot_multiple)
         overtime_hours_worked = hours_worked - reg_hours_worked
         regular_pay = reg_hours_worked * self.__hourly_rate
         overtime_pay = int(overtime_rate * overtime_hours_worked)
         gross_pay = int(regular_pay + overtime_pay)
+        # if gross pay is less than standard_band then return 0 as higher_rate_pay
         higher_rate_pay = 0 if gross_pay < self.__standard_band else gross_pay - \
             self.__standard_band
         standart_tax = int(gross_pay * STANDARD_RATE)
         higher_tax = higher_rate_pay * HIGHER_RATE
         total_tax = standart_tax + higher_tax
         prsi = gross_pay * PRSI_RATE
+        # if tax_credit is greater than total_tax then return 0 as net tax because it cannot be negative
         net_tax = 0 if self.__tax_credit > total_tax else float(
             "{:.1f}".format(total_tax - self.__tax_credit))
         net_deductions = prsi + net_tax
